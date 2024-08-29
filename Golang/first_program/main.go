7defb7688c224e7e28ce2119de39cdaeca158904
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-08-28 21:31:20 -0300

    Update main.go
    Adding functions

--- Golang/first_program/main.go
+++ Golang/first_program/main.go
@@ -1,22 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
-
+	exibeMenu()
 	//first method
 	//var reader int
 	//fmt.Scanf("%d", &reader)
 
 	//second method to get
-	var reader int
-	fmt.Scan(&reader)
-
-	fmt.Println("O comando escolhido foi: ", reader)
 
 	//if reader == 1 {
 	//	fmt.Println("Monitorando....")
@@ -27,7 +23,7 @@ func main() {
 	//} else {
 	//		fmt.Println("Não reconheço este comando")
 	//	}
-
+	reader := leComando()
 	switch reader {
 	case 1:
 		fmt.Println("Monitorando....")
@@ -35,8 +31,28 @@ func main() {
 		fmt.Println("Exibindo Logs....")
 	case 0:
 		fmt.Println("Saindo do Programa")
+		saiPrograma()
 	default:
 		fmt.Println("Não reconheço este comando")
 	}
 
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+}
+
+func leComando() int {
+	var reader int
+	fmt.Scan(&reader)
+
+	fmt.Println("O comando escolhido foi: ", reader)
+	return reader
+}
+
+func saiPrograma() {
+	os.Exit(0)
 }
