66fcdc998000318405664d50476b327a5611f084
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-08-30 18:49:26 -0300

    Update main.go
    Adding monitoring function

--- Golang/first_program/main.go
+++ Golang/first_program/main.go
@@ -2,38 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
-	exibeMenu()
-	//first method
-	//var reader int
-	//fmt.Scanf("%d", &reader)
+	for {
 
-	//second method to get
+		exibeMenu()
+		//first method
+		//var reader int
+		//fmt.Scanf("%d", &reader)
 
-	//if reader == 1 {
-	//	fmt.Println("Monitorando....")
-	//} else if reader == 2 {
-	//		fmt.Println("Exibindo Logs....")
-	//} else if reader == 0 {
-	//		fmt.Println("Saindo do Programa")
-	//} else {
-	//		fmt.Println("Não reconheço este comando")
-	//	}
-	reader := leComando()
-	switch reader {
-	case 1:
-		fmt.Println("Monitorando....")
-	case 2:
-		fmt.Println("Exibindo Logs....")
-	case 0:
-		fmt.Println("Saindo do Programa")
-		saiPrograma()
-	default:
-		fmt.Println("Não reconheço este comando")
+		//second method to get
+
+		//if reader == 1 {
+		//	fmt.Println("Monitorando....")
+		//} else if reader == 2 {
+		//		fmt.Println("Exibindo Logs....")
+		//} else if reader == 0 {
+		//		fmt.Println("Saindo do Programa")
+		//} else {
+		//		fmt.Println("Não reconheço este comando")
+		//	}
+		reader := leComando()
+		switch reader {
+		case 1:
+			IniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs....")
+		case 0:
+			fmt.Println("Saindo do Programa")
+			saiPrograma()
+		default:
+			fmt.Println("Não reconheço este comando")
+		}
 	}
 
 }
@@ -55,4 +59,17 @@ func leComando() int {
 
 func saiPrograma() {
 	os.Exit(0)
+}
+
+func IniciarMonitoramento() {
+	fmt.Println("Monitorando....")
+	site := "https://www.alura.com.br/"
+	response, _ := http.Get(site)
+	//fmt.Print(response)
+
+	if response.StatusCode == 200 {
+		fmt.Print("Site: ", site, "Carregado com sucesso \n")
+	} else {
+		fmt.Print("Site: ", site, "Está com problemas \n")
+	}
 }
