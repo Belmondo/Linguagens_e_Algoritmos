2f05a2fed1e459ca3b136b0d097fe2729b1a76b9
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-12-28 18:09:23 -0300

    Update main.go
    Getting started with new and pointers

--- Golang/object_oriented/main.go
+++ Golang/object_oriented/main.go
@@ -13,4 +13,10 @@ func main() {
 	contaTeste := ContaCorrente{titular: "Belmondo", numeroAgencia: 123456, numeroConta: 654321, saldo: 70.00}
 	fmt.Println(contaTeste)
 
+	var contaTesteDois *ContaCorrente
+	contaTesteDois = new(ContaCorrente)
+	contaTesteDois.titular = "Jr"
+	contaTesteDois.saldo = 500
+
+	fmt.Println(*contaTesteDois)
 }
