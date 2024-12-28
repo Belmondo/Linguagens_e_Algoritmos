862c11a287eccd8b83d2a693873d59bc15a90c2b
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-12-28 18:41:33 -0300

    Update main.go
    Adding functions

--- Golang/object_oriented/main.go
+++ Golang/object_oriented/main.go
@@ -9,14 +9,45 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo Insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	} else {
+		return "Depósito não permitido", c.saldo
+	}
+}
+
 func main() {
-	contaTeste := ContaCorrente{titular: "Belmondo", numeroAgencia: 123456, numeroConta: 654321, saldo: 70.00}
-	fmt.Println(contaTeste)
+	// contaTeste := ContaCorrente{titular: "Belmondo", numeroAgencia: 123456, numeroConta: 654321, saldo: 70.00}
+	// fmt.Println(contaTeste)
 
-	var contaTesteDois *ContaCorrente
-	contaTesteDois = new(ContaCorrente)
-	contaTesteDois.titular = "Jr"
-	contaTesteDois.saldo = 500
+	// var contaTesteDois *ContaCorrente
+	// contaTesteDois = new(ContaCorrente)
+	// contaTesteDois.titular = "Jr"
+	// contaTesteDois.saldo = 500
 
-	fmt.Println(*contaTesteDois)
+	// fmt.Println(*contaTesteDois)
+
+	contaTesteTres := ContaCorrente{}
+	contaTesteTres.titular = "Aragorn"
+	contaTesteTres.saldo = 1000
+
+	fmt.Println(contaTesteTres.saldo)
+	fmt.Println(contaTesteTres.Sacar(300))
+	fmt.Println(contaTesteTres.saldo)
+
+	status, saldoAtual := contaTesteTres.Depositar(500)
+	fmt.Println(status)
+	fmt.Println("Saldo Atual", saldoAtual)
 }
