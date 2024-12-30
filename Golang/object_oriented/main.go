e43f91948b4c0faf98916c96f4280349699080ca
Belmondo Rodrigues <belmondo.rodrigues@gmail.com>
2024-12-30 13:54:25 -0300

    Packages and Visibility
    Adding new package and change variable visibility

--- Golang/object_oriented/main.go
+++ Golang/object_oriented/main.go
@@ -1,53 +1,39 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo Insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Depósito realizado com sucesso", c.saldo
-	} else {
-		return "Depósito não permitido", c.saldo
-	}
-}
+import (
+	"fmt"
+	"object_oriented/contas"
+)
 
 func main() {
-	// contaTeste := ContaCorrente{titular: "Belmondo", numeroAgencia: 123456, numeroConta: 654321, saldo: 70.00}
+	// contaTeste := ContaCorrente{Titular: "Belmondo", numeroAgencia: 123456, numeroConta: 654321, Saldo: 70.00}
 	// fmt.Println(contaTeste)
 
 	// var contaTesteDois *ContaCorrente
 	// contaTesteDois = new(ContaCorrente)
-	// contaTesteDois.titular = "Jr"
-	// contaTesteDois.saldo = 500
+	// contaTesteDois.Titular = "Jr"
+	// contaTesteDois.Saldo = 500
 
 	// fmt.Println(*contaTesteDois)
 
-	contaTesteTres := ContaCorrente{}
-	contaTesteTres.titular = "Aragorn"
-	contaTesteTres.saldo = 1000
+	// contaTesteTres := ContaCorrente{}
+	// contaTesteTres.Titular = "Aragorn"
+	// contaTesteTres.Saldo = 1000
 
-	fmt.Println(contaTesteTres.saldo)
-	fmt.Println(contaTesteTres.Sacar(300))
-	fmt.Println(contaTesteTres.saldo)
+	// fmt.Println(contaTesteTres.Saldo)
+	// fmt.Println(contaTesteTres.Sacar(300))
+	// fmt.Println(contaTesteTres.Saldo)
 
-	status, saldoAtual := contaTesteTres.Depositar(500)
-	fmt.Println(status)
-	fmt.Println("Saldo Atual", saldoAtual)
+	// status, SaldoAtual := contaTesteTres.Depositar(500)
+	// fmt.Println(status)
+	// fmt.Println("Saldo Atual", SaldoAtual)
+
+	contaAragorn := contas.ContaCorrente{Titular: "Aragorn", Saldo: 300}
+	contaLegolas := contas.ContaCorrente{Titular: "Legolas", Saldo: 100}
+
+	statusTransferencia := contaAragorn.Transferir(200, &contaLegolas)
+
+	fmt.Println(statusTransferencia)
+	fmt.Println(contaAragorn)
+	fmt.Println(contaLegolas)
 }
